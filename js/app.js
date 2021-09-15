a5c83b76681ab9843feaacfffee36309fd0fd74c
prateekch33 <prateekchandra27@outlook.com>
2021-09-16 02:13:05 +0530

    Update favicon

--- js/app.js
+++ js/app.js
@@ -165,33 +165,3 @@ const app = new Vue({
         };
     }
 });
-
-function show(segment) {
-    var cult = document.querySelectorAll('./event.html#cult');
-    var tech = document.querySelectorAll('./event.html#tech');
-    var sports = document.querySelectorAll('./event.html#sports');
-    if (segment === 'cult') {
-        for (var i = 0; i < cult.length; i++)
-            cult[i].style.display = 'inline-block';
-        for (var i = 0; i < tech.length; i++)
-            tech[i].style.display = 'none';
-        for (var i = 0; i < sports.length; i++)
-            sports[i].style.display = 'none';
-    }
-    else if (segment === 'tech') {
-        for (var i = 0; i < cult.length; i++)
-            cult[i].style.display = 'none';
-        for (var i = 0; i < tech.length; i++)
-            tech[i].style.display = 'inline-block';
-        for (var i = 0; i < sports.length; i++)
-            sports[i].style.display = 'none';
-    }
-    else {
-        for (var i = 0; i < cult.length; i++)
-            cult[i].style.display = 'none';
-        for (var i = 0; i < tech.length; i++)
-            tech[i].style.display = 'none';
-        for (var i = 0; i < sports.length; i++)
-            sports[i].style.display = 'inline-block';
-    }
-}
